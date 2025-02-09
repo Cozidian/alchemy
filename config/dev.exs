308a6a2254c0255e7c175992e55d40566fb6f547
Christoffer Berglund <christoffer.berglund@icloud.com>
2025-02-09 12:17:30 +0100

    Move env variables to system

--- config/dev.exs
+++ config/dev.exs
@@ -7,10 +7,11 @@ config :alchemy, :ollama_api, "http://localhost:11434/api/generate"
 config :alchemy, :ollama_timeout, 30_000
 
 config :alchemy, ALCHEMY.Repo,
-  database: "alchemy_dev",
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  # "alchemy_dev",
+  database: System.get_env("ALCHEMY_DB_NAME"),
+  username: System.get_env("ALCHEMY_DB_USERNAME"),
+  password: System.get_env("ALCHEMY_DB_PASSWORD"),
+  hostname: System.get_env("ALCHEMY_DB_HOST"),
   port: 5432,
   types: ALCHEMY.PostgrexTypes,
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -3,10 +3,10 @@ import Config
 config :alchemy, :start_file_watcher, false
 
 config :alchemy, ALCHEMY.Repo,
-  database: "alchemy_test",
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
+  database: System.get_env("ALCHEMY_DB_NAME"),
+  username: System.get_env("ALCHEMY_DB_USERNAME"),
+  password: System.get_env("ALCHEMY_DB_PASSWORD"),
+  hostname: System.get_env("ALCHEMY_DB_HOST"),
   port: 5432,
   pool_size: 10,
   types: ALCHEMY.PostgrexTypes
